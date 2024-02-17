a4c532c6c544a821c4cfa0a0dd147029a5c18e6c
Kajitsy <kajitsy@mail.ru>
2024-02-17 15:07:21 +0300

    Lite Update

---
+++ download.bat
@@ -0,0 +1,13 @@
+@echo Loading libraries... 
+@echo off 
+pip install pyfiglet
+pip install whisper-mic
+pip install characterai
+pip install sounddevice
+pip install gpytranslate
+pip install windows-curses
+pip install colorama
+pip install num2words
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+pause
+@echo Press any key to continue. . .
